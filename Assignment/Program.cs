ffa85c9739ba5aae9d8d931f8d6c43d5c26b89d2
Sivabalan M <953618104036@ritrjpm.ac.in>
2022-03-02 16:56:15 +0530

    First-App

---
+++ Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of laptops sold on current month");
+            int NumberOfLaptops=int.Parse(Console.ReadLine());
+            Console.WriteLine("Total sales amount on current month");
+            int SalesAmount=int.Parse(Console.ReadLine());
+            int BasicSalary = 5000;
+            int CommissionAmount = NumberOfLaptops * 1000;
+            int Bonus = (SalesAmount * 2) / 100;
+            int GrossSalary = BasicSalary + CommissionAmount + Bonus;
+            Console.WriteLine("Basic Salary " + BasicSalary);
+            Console.WriteLine("Commission Amount " + CommissionAmount);
+            Console.WriteLine("2% Sales Bonus for " + SalesAmount + " is " + Bonus);
+            Console.WriteLine("Gross Salary " + GrossSalary);
+            Console.ReadKey();
+
+        }
+    }
+}
